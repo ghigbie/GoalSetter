96162f9fb0313a58adbae66a7726786a96c9d0a1
ghigbie <gehigbie@gmail.com>
2020-06-19 10:28:04 -0700

    Connect actions to reducer

--- store/goals.js
+++ store/goals.js
@@ -1,17 +1,18 @@
+import {ADD_GOAL, DELETE_GOAL} from './../actions/goals';
+
 const initialState = {
   goals: [{id: 1, goal: 'Walk puppy!'}, {id: 2, goal: 'Feed kitty.'}],
 };
 
 const goalsReducer = (state = initialState, action) => {
   switch (action.type) {
-    case DELETE_GOAL:
-      return state;
     case ADD_GOAL:
+      return state;
+    case DELETE_GOAL:
       return state;
     default:
       return state;
   }
-  return state;
 };
 
 export default goalsReducer;
